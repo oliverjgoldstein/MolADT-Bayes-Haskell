1294bc20995641418fc635259145277b4a5b8b2a
Oliver J Goldstein <oliverjgoldstein@users.noreply.github.com>
2026-03-29 20:13:01 +0100

    Refresh Haskell Nix shell for current dependencies

--- shell.nix
+++ shell.nix
@@ -1,24 +1,34 @@
 { pkgs ? import <nixpkgs> {} }:
 let
   ghc = pkgs.haskell.packages.ghc96.ghcWithPackages
-          (p: [ p.monad-extras
-                p.transformers
-                p.mtl
-                p.deepseq
+          (p: [ p.array
+                p.binary
+                p.bytestring
                 p.containers
-                p.ghc-heap
-                p.megaparsec
-                p.vector
+                p.deepseq
                 p.directory
                 p.filepath
-                p.bytestring
-                p.text
-                p.random
+                p.ghc-heap
                 p.log-domain
+                p.massiv
+                p.math-functions
+                p.megaparsec
+                p.monad-extras
+                p.mtl
+                p.parallel
+                p.random
                 p.statistics
-                p.array
+                p.text
+                p.time
+                p.transformers
+                p.vector
               ]);
 in
 pkgs.mkShell {
-  buildInputs = [ ghc pkgs.cabal-install pkgs.ripgrep ];
+  buildInputs = [
+    ghc
+    pkgs.cabal-install
+    pkgs.ripgrep
+    pkgs.stack
+  ];
 }
